5be6d357b19b0daf8c2d15294b3d5391602da593
Łukasz Herman <lukaszherman@ukaszs-MacBook-Pro.local>
2020-04-03 20:03:27 +0200

    RB8-2 dynamic ingredient display

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,20 @@ import {BurgerIngredient} from "./BurgerIngredient/BurgerIngredient";
 
 const Burger = (props) => {
 
+    const ingredientEntities = Object.entries(props.ingredients)
+    const ingredientLayers = ingredientEntities.reduce((prev, cur) => {
+        let [type, number] = cur;
+        let newCur = [].concat(...Array(number).fill([type]));
+        return prev.concat(newCur);
+    },[]);
+
+    const ingredientsDisplayed = ingredientLayers.map((ingredient,  i) => <BurgerIngredient key={`layer-${i}`} type={ingredient} />);
+
     return <div className={classes.Burger}>
-        <BurgerIngredient type="bread-top" />
-        <BurgerIngredient type="cheese" />
-        <BurgerIngredient type="meat" />
-        <BurgerIngredient type="bread-bottom" />
-    </div>;
+            <BurgerIngredient type="bread-top" key="bread-1"/>
+              {ingredientsDisplayed}
+            <BurgerIngredient type="bread-bottom" key="bread-2" />
+        </div>;
 };
 
 export {Burger};
